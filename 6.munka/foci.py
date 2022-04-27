3e5bcd32c21ab539bf97230e69c5067c6ccd7308
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-04-27 10:33:24 +0200

    Órai Munka
    Órai munka

--- 6.munka/foci.py
+++ 6.munka/foci.py
@@ -25,8 +25,8 @@ f = open("meccs.txt", 'r', encoding='utf-8')
 f.readline()
 osszesEredmeny: list[Focimeccs] = []
 
-# 1. feladat --------------------------------------------------------------------------------
-print("\n-1. feladat-\n")
+# 1. feladat -------------------------------------------------------------------------------
+print("\n----1. feladat----\n")
 
 
 for sor in f:
@@ -35,7 +35,7 @@ for sor in f:
 print(len(osszesEredmeny))
 
 # 2. feladat --------------------------------------------------------------------------------
-print("\n-2. feladat-\n")
+print("\n----2. feladat----\n")
 
 
 forduloInput = int(input("Forduló száma? (1 és 20 között) "))
@@ -44,3 +44,34 @@ for egyAdat in osszesEredmeny:
     if egyAdat.fordulo == forduloInput:
         print(egyAdat.hazaiCsapat,"-",egyAdat.vendegCsapat,": ",egyAdat.hazaiGol,"-",egyAdat.vendegGol," (",egyAdat.hazaiFelidoGol,"-",egyAdat.vendegFelidoGol,")",sep='')
         
+
+# 3. feladat --------------------------------------------------------------------------------
+print("\n----3. feladat----\n")
+
+keresett_csapat = input("Adjon meg egy csapatnevet!")
+Van_e = False
+for egyAdat in osszesEredmeny:
+    if keresett_csapat == egyAdat.hazaiCsapat or keresett_csapat == egyAdat.vendegCsapat:
+      Van_e = True
+print(Van_e,"Igen,szerepel benne")
+
+
+#4. feladat: 1)Határozza meg, hogy a meccs során mely csapatoknak sikerült megfordítani a meccs állását
+print("\n----4. feladat----\n")
+
+for egyAdat in osszesEredmeny:
+    if egyAdat.hazaiGol > egyAdat.vendegGol or egyAdat.hazaiGol < egyAdat.vendegGol and egyAdat.hazaiFelidoGol or egyAdat.vendegFelidoGol:
+        print(egyAdat.hazaiCsapat,',', egyAdat.vendegCsapat,sep =',')
+
+# 5.feladat: Hány gólt lőttek a nyulak vendéggént
+
+print("\n----5. feladat----\n")
+gol = 0
+for egyAdat in osszesEredmeny:
+    if egyAdat.vendegCsapat == 'Nyulak':
+        gol += egyAdat.vendegGol
+print(gol)
+
+#vagy
+#def forditottake(a:int,b:int,c:int,d:int,)--> bool:
+    #return (c > d and b > a) or (d > c and a > b)
