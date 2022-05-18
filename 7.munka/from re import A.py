2fa8df2bc73d03b046c200155901825b5d61755d
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-05-18 08:24:47 +0200

    1

---
+++ 7.munka/from re import A.py
@@ -0,0 +1,12 @@
+class Buszjarat:
+    majom:int
+    kenyer:int
+    fa:int
+
+    def __init__(self,sor:str)->None:
+        adatok = sor.split(' ')
+        self.majom = int(adatok[0])
+        self.majom = int(adatok[0])
+        self.majom = int(adatok[0])
+
+file = open('txt.txt','r',)
